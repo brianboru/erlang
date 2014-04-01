3e00f8fd00ddfa6b64e27fecd7329d6db580e110
Brian Boyle <email@brianboyle.ie>
2014-04-01 12:14:18 +0100

    Added gitignore to exclude beam files and update eunit test samples

--- ch4/mylists_tests.erl
+++ ch4/mylists_tests.erl
@@ -1,6 +1,18 @@
 -module(mylists_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+% This module uses eunit to perform some basic tests against
+% our lists module
+%
+% This demonstrates how to seperate tests into new module
+% with eunit
+%
+% To run tests simply execute the following command:
+%	eunit:test(mylists)
+%
+% EUnit automatically looks for a test method within
+% modules with name of mylists or mylists_tests
+%
 sum_empty_list_test() ->
 	?assertEqual(0 , mylists:sum([])).
 
