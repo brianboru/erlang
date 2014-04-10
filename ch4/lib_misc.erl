b5adbf2fe9328f47ede7bf6166e108d8cbcc6c66
Brian Boyle <email@brianboyle.ie>
2014-04-10 16:34:21 +0100

    Added some of the list comprehension examples

--- ch4/lib_misc.erl
+++ ch4/lib_misc.erl
@@ -1,5 +1,22 @@
 -module(lib_misc).
--export([for/3]).
+-export([for/3, qsort/1, perms/1]).
 
+%
+% Erlang for loop implementation
 for(Max, Max, F) -> [ F(Max) ];
 for(I, Max, F) -> [F(I) | for(I+1, Max, F)].
+
+
+% 
+% quick sort alogrithm
+qsort([]) -> [];
+qsort([Pivot | T]) ->  					% split list into head and rest of list
+	qsort([X || X <- T, X < Pivot]) 	% put all items less than pivot into new list and sort that, pre-pend it to pivot
+	++ [Pivot] ++						% next have pivot
+	qsort([X || X <- T, X >= Pivot]).	% put all items
+
+%
+% anagrams - permutations sample
+% page 63
+perms([]) -> [[]];
+perms(L) -> [ [H|T] || H <- L, T <- perms(L--[H])].
