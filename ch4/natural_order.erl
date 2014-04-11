24a80ecbb89bb6dd047206ffb47d9a0399cca168
Brian Boyle <email@brianboyle.ie>
2014-04-11 15:13:33 +0100

    updated natural order map function

--- ch4/natural_order.erl
+++ ch4/natural_order.erl
@@ -1,7 +1,15 @@
 -module(natural_order).
--export([map/3]).
+-export([map/2]).
 
+%
+% entry point method
+map(L, Map) ->
+	map(L, Map, []). 	% accumulator is empty list to start with
+
+%
+% private implementation
 map([H|T], Map, Result) ->
-	H1 = Map(H),
-	map(T, Map, [H1 | Result]);
-map([], Map, Result) -> Result.
+	H1 = Map(H),				% run the mapping function
+	map(T, Map, [H1 | Result]);	% put new value on head of result list
+map([], Map, Result) -> lists:reverse(Result).	% once all items are processed return 
+												% result list reversed to restore input order
