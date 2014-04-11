c4c82a5f1f77a4cfe79410ac1f8fdf7db306caf4
Brian Boyle <email@brianboyle.ie>
2014-04-11 14:22:40 +0100

    added guard clause information

--- ch4/guards.erl
+++ ch4/guards.erl
@@ -1,5 +1,5 @@
 -module(guards).
--export([max/2, guard_series/2]).
+-export([max/2, guard_series/2, guard_seq/2]).
 
 
 % 
@@ -12,6 +12,11 @@ max(X,Y) -> Y.
 % guard sequence is a single or series of guards separated by semi-colons (result is true if one is true)
 % guard is a series of guard expressions separated by commas (result is true if all are true)
 
+
+%
+% guard sequence - only one need to evaluate to true
+guard_seq(X,Y) when is_integer(X); Y < 6 -> "X is a number or Y is less than 6";
+guard_seq(X,Y) -> "other side".
 
 % 
 % guard sample
