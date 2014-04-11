2aee3ef711e4c301ce74457f6c6208a8723392d0
Brian Boyle <email@brianboyle.ie>
2014-04-11 11:50:02 +0100

    added comments to help understand perms in lib_misc.erl

--- ch4/lib_misc.erl
+++ ch4/lib_misc.erl
@@ -18,5 +18,11 @@ qsort([Pivot | T]) ->  					% split list into head and rest of list
 %
 % anagrams - permutations sample
 % page 63
-perms([]) -> [[]];
-perms(L) -> [ [H|T] || H <- L, T <- perms(L--[H])].
+% this makes my head hurt
+perms([]) -> [[]];						% okay so perms of empty is empty - that bit is fine
+perms(L) -> [ [H|T] || H <- L, T <- perms(L--[H])].	% so the [H|T] || H <- L bit generates a list with each item so "123" -> ["1", "2", "3"]
+													% T will be perms of remaining list without the first number i.e.
+													% "123" => ["1"] + perms["23"] 	=> "23" => ["2"] + perms["3"]
+													%										=> ["3"] + perms["2"]
+													%		=> ["2"] + perms["13"]
+													%		=> ["3"] + perms["12"]
